d13d53ab821f044234975fcaa1a99cdfde6b33ec
Reed Cannon <reed.cannon97@gmail.com>
2022-05-16 14:28:28 -0600

    Created some class outlines for Loot and Enemy

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace miniRPG
+{
+    /// <summary>
+    /// The program's entry point.
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Starts the program using the given arguments.
+        /// </summary>
+        /// <param name="args">The given arguments.</param>
+        static void Main(string[] args)
+        {
+            Director director = new Director();
+            director.StartGame();
+        }
+    }
+}
---
+++ game/Director.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+
+
+namespace minRPG
+{
+    /// <summary>
+    /// A person who directs the game. 
+    ///
+    /// The responsibility of a Director is to control the sequence of play.
+    /// </summary>
+    public class Director
+    {
+        //List<Dealer> cards = new List<Dealer>();
+
+
+
+        public Director()
+        {
+                //This whole method should be blank
+        }
+        
+
+        public void StartGame()
+        {
+            
+        }
+
+        public void GetInputs()
+        {
+
+            
+        }
+
+
+        public void DoUpdates()
+        {
+
+        }
+
+
+        public void DoOutputs()
+        {
+
+        }
+    }
+}
